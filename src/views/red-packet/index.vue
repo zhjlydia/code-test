e629e62f0052212df69bdb1d08a5cd39879efa32
zhjlydia <zhjlydia@foxmail.com>
2021-06-27 22:38:57 +0800

    chore: update

---
+++ src/views/red-packet/index.vue
@@ -0,0 +1,14 @@
+<!-- @format -->
+
+<template>
+  
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AlignCenter extends Vue {}
+</script>
+<style lang="less" scoped>
+
+</style>
